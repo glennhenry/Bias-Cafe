63e9e06f838a7085892c8cb88108ed91c7ec38f5
glennhenry <glennhenrybdg05@gmail.com>
2026-06-30 18:14:05 +0700

    Add 2 custom config for topic repository

--- src/main/kotlin/project/config/CustomConfig.kt
+++ src/main/kotlin/project/config/CustomConfig.kt
@@ -1,6 +1,7 @@
 package project.config
 
 import encore.annotation.runtime.VenueKey
+import project.domain.cafe.topic.MongoTopicRepository
 
 /**
  * Custom application config definition.
@@ -11,5 +12,22 @@ import encore.annotation.runtime.VenueKey
  * All field is preferred to be immutable.
  */
 data class CustomConfig(
-    val dummy: Int = 0
+    /**
+     * Whether to prepare dummy posts for [MongoTopicRepository].
+     *
+     * This will insert 20 posts during server startup if
+     * fewer than 10 posts are available.
+     */
+    @VenueKey("prepareDummyPosts")
+    val prepareDummyPosts: Boolean = false,
+
+    /**
+     * Whether to delete all stored posts in the topic collection
+     * of [MongoTopicRepository] on every server restart.
+     *
+     * Deletion will be done first before adding the posts
+     * if [prepareDummyPosts] is `true`.
+     */
+    @VenueKey("deletePostsEveryRestart")
+    val deletePostsEveryRestart: Boolean = false
 )
