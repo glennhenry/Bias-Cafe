525091c551c28c13a35119145a2d95ea4c3d27b8
glennhenry <glennhenrybdg05@gmail.com>
2026-07-28 14:15:18 +0700

    Create placeholder.png and serve

--- src/main/kotlin/project/routes/FileRoutes.kt
+++ src/main/kotlin/project/routes/FileRoutes.kt
@@ -20,6 +20,7 @@ fun Route.fileRoutes() {
     // serve site assets
     staticFiles("site", File("assets/site"))
     staticFiles("icons", File("assets/icons"))
+    staticFiles("images", File("assets/images"))
     get("/favicon.ico") { call.respondFile(File("assets/site/favicon.ico")) }
 
     val docsDir = File("docs_build")
